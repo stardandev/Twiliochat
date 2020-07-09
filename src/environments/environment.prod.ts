e08393e9b45662932ea248fdc7b49a4bd579da8c
= <=>
2020-07-09 09:58:38 +0100

    Config Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDcmgoVtg_6q24DXxnXnL06h9zIltpnM_I',
+    authDomain: 'chat-5ad86.firebaseapp.com',
+    databaseURL: 'https://chat-5ad86.firebaseio.com',
+    projectId: 'chat-5ad86',
+    storageBucket: 'chat-5ad86.appspot.com',
+    messagingSenderId: '44371662093',
+    appId: '1:44371662093:web:7dede7d6d0cec6603c4c44',
+    measurementId: 'G-0PKYST240P',
+  },
 };
